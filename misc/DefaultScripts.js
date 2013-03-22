f39eef089ff49e131e431b1c8175f22a63779591
Danny <hisoka365@gmail.com>
2013-03-21 21:57:00 -0400

    Added Student_Wait page

---
+++ misc/DefaultScripts.js
@@ -0,0 +1,45 @@
+function getDate()
+{
+	var today = new Date();
+	var day   = today.getDate();
+	var month = today.getMonth();
+	var year  = today.getYear(); 
+	
+	document.write("<b>" + day + "/" + month + "/" + year + "</b>")
+} // End getDate()
+
+function getTime24()
+{
+	var time    = new Date();
+	var hours   = time.getHours();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds();
+	
+	if (minutes < 10)
+		minutes = "0" + minutes;
+		
+		document.write("<b>" + hours + ":" + minutes + ":" seconds + "</b>");
+}
+
+function getTime12()
+{
+	var time    = new Date();
+	var hours   = time.getHours();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds();
+	
+	if (minutes < 10)
+		minutes = "0" + minutes;
+		
+	var meridiem = "AM";
+	if (hours >= 12)
+	{
+		meridiem = "PM";
+		hours = hours - 12;
+	}
+	
+	if (hours == 0)
+		hours = 12;
+	
+	document.write("<b>" + hours + ":" + minutes + ":" + secondd + " " + meridiem + "</b>"); 
+}
